e40e0647c9796f3f035271e3bec577878879abd4
Witold Wasiczko <witold@wasiczko.pl>
2021-12-09 15:07:58 +0100

    PHP 8.1 compatibility
    Signed-off-by: Witold Wasiczko <witold@wasiczko.pl>

--- test/TestAsset/ModelWithJson.php
+++ test/TestAsset/ModelWithJson.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace LaminasTest\ApiTools\ContentNegotiation\TestAsset;
 
 use Laminas\Stdlib\JsonSerializable;
+use ReturnTypeWillChange;
 
 class ModelWithJson implements JsonSerializable
 {
     /** @return mixed */
+    #[ReturnTypeWillChange]
     public function jsonSerialize()
     {
         return ['foo' => 'bar'];
